7f68bad8f1d1149890a7fbd3aeb0542c5be26ec3
jc.bernack <jc.bernack@gmail.com>
2016-12-11 15:51:30 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+
+const app = express();
+app.use(helmet());
+app.use(express.static("."));
+
+const port = process.env.PORT || 80;
+const server = app.listen(port, function () {
+  console.log("Server listening on", server.address());
+});
