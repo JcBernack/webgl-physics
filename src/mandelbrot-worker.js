43ca287c731768f408c8bae681ed12bf0d76573a
jc.bernack <jc.bernack@gmail.com>
2016-12-30 16:45:07 +0100

    Add smooth coloring

--- src/mandelbrot-worker.js
+++ src/mandelbrot-worker.js
@@ -1,3 +1,7 @@
+var log2 = Math.log(2);
+var escape = 2 << 16;
+var escape2 = escape*escape;
+
 function message(e) {
     var d = e.data;
     d.result = [];
@@ -8,10 +12,15 @@ function message(e) {
             var zre = cre;
             var zim = cim;
             // iterate Z(i+1) = Z(i)^2 + C as long as |Z| < 2 and i < max
-            for (var i = 0; i < d.iterations && (zre*zre+zim*zim) < 4; i++) {
+            for (var i = 0; i < d.iterations && (zre*zre + zim*zim) < escape2; i++) {
                 var tmp = zre*zre - zim*zim + cre;
                 zim = 2*zre*zim + cim;
                 zre = tmp;
+            }
+            // source: https://en.wikipedia.org/wiki/Mandelbrot_set#Continuous_.28smooth.29_coloring
+            if (i < d.iterations) {
+                var log_z = Math.log(zre*zre + zim*zim) / 2;
+                i = i + 1 - Math.log(log_z / log2) / log2;
             }
             d.result.push(i);
         }
