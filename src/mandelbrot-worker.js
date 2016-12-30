15883119e55e16d26be6b8fa453b8a6eaa6f2655
jc.bernack <jc.bernack@gmail.com>
2016-12-30 05:37:04 +0100

    Mutable complex numbers

--- src/mandelbrot-worker.js
+++ src/mandelbrot-worker.js
@@ -4,23 +4,23 @@ function C(re, im) {
         im: im
     };
     that.add = function(c) {
-        return C(re + c.re, im + c.im);
+        that.re += c.re;
+        that.im += c.im;
+        return that;
     };
-    // that.sub = function(c) {
-    //     return C(re - c.re, im - c.im);
-    // };
-    // that.mul = function(c) {
-    //     // (a+bi)*(c+di)
-    //     // = (ac-bd) + (ad+bc)i
-    //     return C(re*c.re - im*c.im, re*c.im + im*c.re);
-    // };
     that.square = function() {
         // (a+bi)*(a+bi)
         // = (a^2-b^2) + (2ab)i
-        return C(re*re - im*im, 2*re*im);
+        var tmp = that.re*that.re - that.im*that.im;
+        that.im = 2*that.re*that.im;
+        that.re = tmp;
+        return that;
     };
     that.lengthSquared = function() {
-        return re*re + im*im;
+        return that.re*that.re + that.im*that.im;
+    };
+    that.copy = function() {
+        return C(that.re, that.im);
     };
     return that;
 }
@@ -32,24 +32,23 @@ function message(e) {
     for (var y = 0; y < d.height; y++) {
         var im = d.im_start + y * d.im_range / d.height;
         for (var x = 0; x < d.width; x++) {
-            // var c = C(d.re_start + x * d.re_range / d.width, im);
-            // var z = c;
-            // // iterate Z(i+1) = Z(i)^2 + C as long as |Z| < 2 and i < max
-            // for (var i = 0; i < d.iterations && z.lengthSquared() < 4; i++) {
-            //     z = z.square().add(c);
-            // }
-
-            var cre = d.re_start + x * d.re_range / d.width;
-            var cim = im;
-            var zre = cre;
-            var zim = cim;
+            var c = C(d.re_start + x * d.re_range / d.width, im);
+            var z = c.copy();
             // iterate Z(i+1) = Z(i)^2 + C as long as |Z| < 2 and i < max
-            for (var i = 0; i < d.iterations && (zre*zre+zim*zim) < 4; i++) {
-                var tmp = zre*zre - zim*zim + cre;
-                zim = 2*zre*zim + cim;
-                zre = tmp;
+            for (var i = 0; i < d.iterations && z.lengthSquared() < 4; i++) {
+                z.square().add(c);
             }
 
+            // var cre = d.re_start + x * d.re_range / d.width;
+            // var cim = im;
+            // var zre = cre;
+            // var zim = cim;
+            // // iterate Z(i+1) = Z(i)^2 + C as long as |Z| < 2 and i < max
+            // for (var i = 0; i < d.iterations && (zre*zre+zim*zim) < 4; i++) {
+            //     var tmp = zre*zre - zim*zim + cre;
+            //     zim = 2*zre*zim + cim;
+            //     zre = tmp;
+            // }
             d.result.push(i);
         }
     }
